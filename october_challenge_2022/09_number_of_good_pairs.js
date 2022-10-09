2601c07c5c7fbc85f73ee01abe9211b944d84871
Abbos Engineer <abbosshamsiddin@gmail.com>
2022-10-09 18:27:12 +0500

    leetcode number of good pairs done

---
+++ october_challenge_2022/09_number_of_good_pairs.js
@@ -0,0 +1,9 @@
+var numIdenticalPairs = function (nums) {
+    let c = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] === nums[j] && i < j) c++;
+        }
+    }
+    return c;
+};
