2ac167ee8603cfc0df1634ac32cae711f8d596e3
Abbos Engineer <abbosshamsiddin@gmail.com>
2023-01-08 12:08:17 +0500

    LeetCode squareIsWhite second solution

--- january_challenge_2023/08_squareIsWhite.js
+++ january_challenge_2023/08_squareIsWhite.js
@@ -17,3 +17,36 @@ var squareIsWhite = function (coordinates) {
         return false;
     }
 };
+
+
+/////////////////////////////
+// second way
+/**
+ * @param {string} coordinates
+ * @return {boolean}
+ */
+var squareIsWhite = function (coordinates) {
+    let letter = coordinates[0];
+    let num = coordinates[1] - 0;
+
+    let letters = {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8,
+    }
+
+    let bool = false;
+
+    for (const l in letters) {
+        let letterIndex = letters[l];
+        if (num % 2 === 0 && l === letter && letterIndex % 2 !== 0) bool = true;
+        if (num % 2 !== 0 && l === letter && letterIndex % 2 === 0) bool = true;
+    }
+
+    return bool;
+};
