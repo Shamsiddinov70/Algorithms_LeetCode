4d3b9f7ea5d651896b0ffac65beff608dc1d3431
Abbos Engineer <abbosshamsiddin@gmail.com>
2023-01-18 10:04:30 +0500

    nextGreatestLetter fixed

--- january_challenge_2023/24_nextGreatestLetter.js
+++ january_challenge_2023/24_nextGreatestLetter.js
@@ -1,15 +1,10 @@
-/**
- * @param {character[]} letters
- * @param {character} target
- * @return {character}
- */
 var nextGreatestLetter = function (letters, target) {
     let alpha = "abcdefghijklmnopqrstuvwxyz";
-    let ans = "";
     for (let i = 0; i < letters.length; i++) {
         if (alpha.indexOf(letters[i]) > alpha.indexOf(target)) {
-            ans += letters[i];
+            return letters[i];
         }
     }
-    return ans.length >= 1 ? ans[0] : letters[0];
+
+    return letters[0];
 };
