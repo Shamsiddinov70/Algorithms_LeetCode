0ab7fba2f3eee8e5038246eb2e3fa370f0c06a4f
Abbos Engineer <abbosshamsiddin@gmail.com>
2023-01-21 13:39:07 +0500

    LeetCode createTargetArray Easy

---
+++ january_challenge_2023/27_createTargetArray.py
@@ -0,0 +1,28 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target = []
+
+        for i in range(len(nums)):
+            target.insert(index[i], nums[i])
+
+        return target
+
+
+# JS solution
+"""
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    let target = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < index.length; j++) {
+            target.splice(index[i], 0, nums[i]);
+            break;
+        }
+    }
+    return target;
+};
+"""
