b2f6705c88e7268dd70e31a73e49887e584f2446
Abbos Engineer <abbosshamsiddin@gmail.com>
2023-01-26 12:49:28 +0500

    LeetCode checkIfExist Easy

--- january_challenge_2023/32_findWords.js
+++ january_challenge_2023/32_findWords.js
@@ -2,7 +2,7 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var findWords = function(words) {
+var findWords = function (words) {
     const firstRow = 'qwertyuiop';
     const secondRow = 'asdfghjkl';
     const thirdRow = 'zxcvbnm';
@@ -33,3 +33,35 @@ var findWords = function(words) {
 
     return res;
 };
+
+//Python solution
+/*
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        first_row = 'qwertyuiop'
+        second_row = 'asdfghjkl'
+        third_row = 'zxcvbnm'
+
+        result = []
+        for word in words:
+            set_letters = list(set(word.lower()))
+
+            first = 0
+            second = 0
+            third = 0
+
+            for i in range(len(set_letters)):
+                if set_letters[i] in first_row:
+                    first += 1
+                if set_letters[i] in second_row:
+                    second += 1
+                if set_letters[i] in third_row:
+                    third += 1
+
+            leng = len(set_letters)
+
+            if leng == first or leng == second or leng == third:
+                result.append(word)
+
+        return result
+*/
