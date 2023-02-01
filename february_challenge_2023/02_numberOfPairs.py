2c28fcdfabd16d8c5b42ad3e6a5af70258261847
Abbos Engineer <abbosshamsiddin@gmail.com>
2023-02-01 17:41:06 +0500

    LeetCode numberOfPairs Easy

---
+++ february_challenge_2023/02_numberOfPairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfPairs(nums):
+        dic = Counter(nums)
+        pairs = 0
+        remain = 0
+
+        for v in dic.values():
+            pairs += v // 2
+
+            if v == 1 or v > 2 and v % 2 != 0:
+                remain += 1
+
+        return [pairs, remain]
