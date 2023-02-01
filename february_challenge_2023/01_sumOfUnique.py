78ada7c5539e42d2762bdaec4664e457b0243c1b
Abbos Engineer <abbosshamsiddin@gmail.com>
2023-02-01 17:09:07 +0500

    LeetCode sumOfUnique Easy

---
+++ february_challenge_2023/01_sumOfUnique.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumOfUnique(nums):
+        dic = Counter(nums)
+        ans = 0
+
+        for key, val in dic.items():
+            if val == 1:
+                ans += int(key)
+
+        return ans
