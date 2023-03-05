15da834258bccabdba426f24f1d486e35d904317
Abbos Engineer <abbosshamsiddin@gmail.com>
2023-03-05 12:46:45 +0500

    LeetCode distinctAverages Easy

---
+++ march_challenge_2023/09_distinctAverages.py
@@ -0,0 +1,12 @@
+class Solution:
+    def distinctAverages(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        ans_set = set()
+        for i in range(len(nums)):
+            while len(nums) != 0:
+                min_num = nums[0:1]
+                max_num = nums.pop()
+                nums.remove(min_num[0])
+                ans_set.add((min_num[0] + max_num) / 2)
+
+        return len(ans_set)
